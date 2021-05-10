357427bc7ee546b3f20c12591b2d98335ccb7e93
Shivani Singh <ssingh94@in.ibm.com>
2021-05-10 21:43:51 +0530

    configuration

---
+++ tenisKata/src/main/java/socgen_test/tenisKata/App.java
@@ -0,0 +1,13 @@
+package socgen_test.tenisKata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
